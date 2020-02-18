e30560c616b94282e697526b34a7391f421320f9
Mohinesh Tayade <mohineshtayade123@gmail.com>
2020-02-18 13:35:16 +0530

    second commit

---
+++ src/com/sth/puzzle/NumberOfDigitsInNumber.java
@@ -0,0 +1,10 @@
+package com.sth.puzzle;
+
+public class NumberOfDigitsInNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter the no");
+	}
+
+}
